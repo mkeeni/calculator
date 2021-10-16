477fdd79c515d386ad85b6d8d575fd2a65abb094
mkeeni <mohnish_201900167@smit.smu.edu.in>
2021-10-16 15:52:12 +0530

    My first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+declare var jQuery: any;
+
+@Component({
+  selector: 'main-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'calculator';
+}
+
